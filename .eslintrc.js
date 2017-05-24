5fee19ba87455de1a9d8132e41455396ba1666cf
ErnestRC <ernest@unstable.build>
2017-05-24 13:42:43 -0700

    modified eslintrc to use airbnb config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  rules: {
+    'comma-dangle': 0,
+    'no-plusplus': 0,
+    'no-cond-assign': 0,
+    'no-continue': 0,
+  },
+  extends: 'airbnb'
+};
