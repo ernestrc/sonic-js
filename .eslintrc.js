f7463cb46d5531d8fc5ce61e895263e02606469b
ErnestRC <ernest@unstable.build>
2017-05-26 12:27:17 -0700

    add connection pool to Client

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,10 @@ module.exports = {
     'no-plusplus': 0,
     'no-cond-assign': 0,
     'no-continue': 0,
+    'no-console': 0,
+    'one-var': 0,
+    'one-var-declaration-per-line': 0,
+    'no-underscore-dangle': 0,
   },
   extends: 'airbnb'
 };
--- src/lib.js
+++ src/lib.js
@@ -2,6 +2,7 @@ const BrowserWebSocket = global.MozWebSocket || global.WebSocket;
 const WebSocket = BrowserWebSocket || require('ws'); // eslint-disable-line global-require
 const EventEmitter = require('events');
 const utils = require('./util');
+const pool = require('generic-pool');
 
 const SonicMessage = utils.SonicMessage;
 const noop = () => {};
@@ -14,147 +15,301 @@ if (BrowserWebSocket) {
   };
 }
 
-function cancel(ws, _cb) {
-  const cb = typeof _cb === 'function' ? _cb : noop;
-  function doSend() {
-    if (BrowserWebSocket) {
-      try {
-        ws.send(SonicMessage.CANCEL);
-        cb();
-      } catch (e) {
-        cb(e);
-      }
-    } else {
-      ws.send(SonicMessage.CANCEL, cb);
-    }
-  }
-  if (ws.readyState === WebSocket.OPEN) {
-    doSend();
-  } else {
-    ws.on('open', doSend);
-  }
-}
-
 class SonicEmitter extends EventEmitter {}
 
+function getCloseError(ev) {
+  return new Error(`WebSocket close: code=${ev.code}; reason=${ev.reason}`);
+}
+
+const states = {
+  INITIALIZED: 1,
+  CLOSING: 2,
+  CLOSED: 3,
+};
+
 class Client {
-  constructor(sonicAddress) {
+  constructor(sonicAddress,
+    { maxPoolSize, minPoolSize, debug, validateTimeout, maxTries, acquireTimeout } = {}) {
     this.url = sonicAddress;
-    this.ws = [];
+    this.running = {};
+    this.nextId = 1;
+    this.state = states.INITIALIZED;
+    this.debug = debug;
+    this.validateTimeout = validateTimeout || 10000;
+    this.maxPoolSize = maxPoolSize || 5;
+    this.minPoolSize = minPoolSize || 1;
+    this.maxTries = maxTries || 1;
+    this.acquireTimeout = acquireTimeout || 2000;
+    this._initializePool();
   }
 
-  doSend(doneCb, outputCb, progressCb, metadataCb, startedCb) {
+  _initializePool() {
+    const client = this;
+    const poolOpts = {
+      max: this.maxPoolSize, // maximum size of the pool
+      min: this.minPoolSize, // minimum size of the pool
+      maxTries: this.maxTries,
+      autostart: true,
+      testOnBorrow: true,
+    };
+
+    const WsFactory = {
+      create() {
+        return new Promise((resolve, reject) => {
+          const ws = new WebSocket(client.url);
+          let onOpen, onClose;
+
+          const onError = (err) => {
+            ws.removeListener('open', onOpen);
+            ws.removeListener('close', onClose);
+            reject(err);
+          };
+
+          onOpen = () => {
+            ws.removeListener('error', onError);
+            ws.removeListener('close', onClose);
+            resolve(ws);
+          };
+
+          onClose = (ev) => {
+            ws.removeListener('error', onError);
+            ws.removeListener('open', onOpen);
+            reject(getCloseError(ev));
+          };
+
+          ws.on('open', onOpen);
+          ws.on('close', onClose);
+          ws.on('error', onError);
+        });
+      },
+
+      validate(ws) {
+        return new Promise((resolve) => {
+          let onPong;
+
+          const timer = setTimeout(() => {
+            resolve(false);
+            ws.removeListener('pong', onPong);
+          }, this.validateTimeout);
+
+          onPong = () => {
+            clearTimeout(timer);
+            resolve(true);
+          };
+
+          ws.on('pong', onPong);
+
+          try {
+            ws.ping();
+          } catch (e) {
+            resolve(false);
+          }
+        });
+      },
+
+      destroy(ws) {
+        return new Promise((resolve) => {
+          ws.on('close', () => {
+            resolve();
+          });
+          ws.close(1000, 'pool#destroy');
+        });
+      },
+    };
+
+    this.pool = pool.createPool(WsFactory, poolOpts);
+  }
+
+  _cancel(id, _cb) {
+    const ws = this.running[id];
+    const cb = typeof _cb === 'function' ? _cb : noop;
+
+    if (this.debug) console.log(`cancelling: WebSocket(${id})`);
+
+    /* a 'D' message is expected when a cancel is send,
+     * therefore resource cleanup should be handled by done handler
+     */
+    if (!ws) {
+      if (this.debug) console.error(new Error(`cancel: WebSocket(${id}) is not running any queries`));
+      cb();
+      return;
+    }
+
+    const doSend = () => {
+      if (!BrowserWebSocket) {
+        ws.send(SonicMessage.CANCEL, () => {
+          if (this.debug) console.log(`cancelled: WebSocket(${id})`);
+          cb();
+        });
+        return;
+      }
+
+      try {
+        ws.send(SonicMessage.CANCEL);
+        if (this.debug) console.log(`cancelled: WebSocket(${id})`);
+        cb();
+      } catch (e) {
+        if (this.debug) console.error(e);
+        cb(e);
+      }
+    };
+
+    if (ws.readyState === WebSocket.CONNECTING) {
+      ws.on('open', doSend);
+    } if (ws.readyState === WebSocket.OPEN) {
+      doSend();
+    } else {
+      // connection is CLOSING/CLOSED
+      cb();
+    }
+  }
+
+  /* ws client agnostic send method */
+  _wsSend(message, doneCb, outputCb, progressCb, metadataCb, startedCb, ws) {
     const output = outputCb || noop;
     const progress = progressCb || noop;
     const metadata = metadataCb || noop;
-    let isDone = false;
-    let isError = false;
+    let onError, onClose, onMessage;
 
-    return (message, ws) => {
-      ws.send(message);
-
-      const done = (err, id) => {
-        let idx;
-
-        ws.close(1000, 'completed');
-
-        if ((idx = this.ws.indexOf(ws)) < 0) {
-          throw new Error('ws not found');
-        }
-
-        this.ws.splice(idx, 1);
-
-        doneCb(err, id);
-      };
-
-      const closedUnexp = () => {
-        done(new Error('connection closed unexpectedly'));
-      };
-
-      ws.on('close', (ev) => {
-      // browser
-        if (BrowserWebSocket) {
-          if (isError) {
-            done(new Error(`WebSocket close code: ${ev.code}; reason: ${ev.reason}`));
-          } else if (ev.code !== 1000 && !isDone) {
-            closedUnexp();
-          }
-
-        // ws
-        } else if (!isDone && ev !== 1000) {
-          closedUnexp();
-        }
-      });
-
-      ws.on('error', (ev) => {
-      // ev is defined with `ws`, but not with the
-      // browser's WebSocket API
-        if (BrowserWebSocket) {
-          isError = true;
-        } else {
-          isDone = true;
-          done(ev);
-        }
-      });
-
-      ws.on('message', (_message) => {
-        const msg = BrowserWebSocket ? JSON.parse(_message.data) : JSON.parse(_message.toString('utf-8'));
-        const checkMsg = () => {
-          if (msg.v) {
-            done(new Error(`Query with trace_id \`${msg.p.trace_id}\` failed: ${msg.v}`));
-          } else {
-            done(null);
-          }
-        };
-
-        switch (msg.e) {
-          case 'P':
-            progress(utils.toProgress(msg.p));
-            break;
-
-          case 'D':
-            isDone = true;
-            if (BrowserWebSocket) {
-              ws.send(SonicMessage.ACK);
-              checkMsg();
-            } else {
-              ws.send(SonicMessage.ACK, checkMsg);
-            }
-            break;
-
-          case 'T':
-            metadata(msg.p.map(elem => [elem[0], typeof elem[1]]));
-            break;
-
-          case 'S':
-            if (typeof startedCb !== 'undefined') {
-              startedCb(msg.v);
-            }
-            break;
-
-          case 'O':
-            output(msg.p);
-            break;
-
-          default:
-          // ignore to improve forwards compatibility
-            break;
-        }
-      });
+    const done = (err) => {
+      ws.removeListener('close', onClose);
+      ws.removeListener('error', onError);
+      ws.removeListener('message', onMessage);
+      doneCb(err);
     };
+
+    onClose = (ev) => {
+      if (this.debug) console.log(`WebSocket closed: code=${ev.code}; reason=${ev.reason};`);
+      if (!ws.sonicError) {
+        done(getCloseError(ev));
+      } else {
+        done(ws.sonicError);
+      }
+    };
+
+    onError = (err) => {
+      if (this.debug) console.error(err);
+      // err is defined with `ws`, but not with the
+      // browser's WebSocket API so we need to get the errors from the close event
+      ws.sonicError = err; // eslint-disable-line no-param-reassign
+    };
+
+    onMessage = (_message) => {
+      const msg = BrowserWebSocket ? JSON.parse(_message.data) : JSON.parse(_message.toString('utf-8'));
+      const completeStream = () => {
+        if (this.debug) console.log(`completed complete/ack sequence: trace_id=${msg.p.trace_id}; error=${msg.v};`);
+        if (msg.v) {
+          done(new Error(`query with trace_id \`${msg.p.trace_id}\` failed: ${msg.v}`));
+        } else {
+          done(null);
+        }
+      };
+
+      switch (msg.e) {
+        case 'P':
+          progress(utils.toProgress(msg.p));
+          break;
+
+        case 'D':
+          if (BrowserWebSocket) {
+            ws.send(SonicMessage.ACK);
+            completeStream();
+          } else {
+            ws.send(SonicMessage.ACK, completeStream);
+          }
+          break;
+
+        case 'T':
+          metadata(msg.p.map(elem => [elem[0], typeof elem[1]]));
+          break;
+
+        case 'S':
+          if (typeof startedCb !== 'undefined') {
+            startedCb(msg.v);
+          }
+          break;
+
+        case 'O':
+          output(msg.p);
+          break;
+
+        default:
+          if (this.debug) console.log(`unsupported message received: ${JSON.stringify(msg)}`);
+          break;
+      }
+    };
+
+    ws.on('close', onClose);
+    ws.on('error', onError);
+    ws.on('message', onMessage);
+    ws.send(message);
+    if (this.debug) console.log(`sending message: ${JSON.stringify(message)}`);
   }
 
-  send(message, doneCb, outputCb, progressCb, metadataCb, startedCb) {
-    const ws = new WebSocket(this.url);
-    const doExec = this.doSend(doneCb, outputCb, progressCb, metadataCb, startedCb);
+  /* pool-aware send method */
+  _send(message, doneCb, outputCb, progressCb, metadataCb, startedCb) {
+    switch (this.state) {
+      case states.CLOSED:
+        doneCb(new Error('client is closed and cannot accept more work'));
+        return;
+      case states.CLOSING:
+        doneCb(new Error('client is closing and cannot accept more work'));
+        return;
+      default:
+    }
 
-    ws.on('open', () => {
-      doExec(JSON.stringify(message), ws);
-    });
+    // identify send for cancel hooks
+    const id = this.nextId++;
+    let doned = false;
 
-    this.ws.push(ws);
+    let timer;
+    if (this.debug) {
+      timer = setTimeout(() =>
+        console.log(`its taking more than (${this.acquireTimeout}) to acquire resource for ticket=${id}`),
+          this.acquireTimeout);
+    }
 
-    return ws;
+    // acquire connection
+    this.pool.acquire().then((ws) => {
+      this.running[id] = ws;
+
+      if (this.debug) {
+        if (!ws.sonicId) {
+          /* first ticket for this resource is the resource ID */
+          ws.sonicId = id; // eslint-disable-line no-param-reassign
+        }
+        console.log(`acquired resource=${ws.sonicId} for ticket=${id}`);
+        clearTimeout(timer);
+      }
+
+      // doneCb override to release connection back to pool
+      const doDoneCb = (err) => {
+        if (this.debug) console.log(`done with ticket=${id}; resource=${ws.sonicId}`);
+        if (!doned) {
+          doned = true;
+          delete this.running[id];
+
+          if (ws.sonicDestroy) {
+            if (this.debug) console.log(`destroying resource=${ws.sonicId}; ticket=${id}`);
+            this.pool.destroy(ws);
+          } else {
+            if (this.debug) console.log(`releasing resource=${ws.sonicId}; ticket=${id}`);
+            this.pool.release(ws);
+          }
+          doneCb(err);
+        }
+      };
+
+      try {
+        this._wsSend(JSON.stringify(message), doDoneCb, outputCb, progressCb, metadataCb, startedCb, ws);
+      } catch (e) {
+        if (this.debug) console.error(e);
+        this.pool.release(ws);
+        doDoneCb(e);
+      }
+    }).catch(doneCb);
+
+    return id; // eslint-disable-line consistent-return
   }
 
   stream(query) {
@@ -186,15 +341,16 @@ class Client {
       emitter.emit('started', traceId);
     }
 
-    const ws = this.send(queryMsg, done, output, progress, metadata, started);
+    const id = this._send(queryMsg, done, output, progress, metadata, started);
 
     emitter.cancel = (cb) => {
-      cancel(ws, cb);
+      this._cancel(id, cb);
     };
 
     return emitter;
   }
 
+  /* TODO: deprecate in favor of run2 */
   run(query, doneCb) {
     const data = [];
     const queryMsg = utils.toMsg(query);
@@ -211,13 +367,19 @@ class Client {
       data.push(elems);
     }
 
-    const ws = this.send(queryMsg, done, output);
+    this._send(queryMsg, done, output);
+  }
 
-    return {
-      cancel(cb) {
-        cancel(ws, cb);
-      },
-    };
+  run2(query) {
+    return new Promise((resolve, reject) => {
+      this.run(query, (err, data) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(data);
+      });
+    });
   }
 
   authenticate(user, apiKey, doneCb, traceId) {
@@ -233,7 +395,7 @@ class Client {
 
     function done(err) {
       if (err) {
-        doneCb(err, null);
+        doneCb(err);
       } else {
         doneCb(null, token);
       }
@@ -243,11 +405,37 @@ class Client {
       token = elems[0];
     }
 
-    this.send(authMsg, done, output);
+    this._send(authMsg, done, output);
+  }
+
+  _close() {
+    if (this.state !== states.CLOSING) {
+      return Promise.resolve();
+    }
+    return this.pool.drain().then(() => this.pool.clear());
+  }
+
+  cancel() {
+    const ids = Object.keys(this.running);
+
+    return Promise.all(ids.map(id => new Promise((resolve, reject) => {
+      this._cancel(id, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    })));
   }
 
   close() {
-    this.ws.map(cancel);
+    this.state = states.CLOSING;
+    return this.cancel()
+      .then(() => this._close())
+      .then(() => {
+        this.state = states.CLOSED;
+      });
   }
 }
 
